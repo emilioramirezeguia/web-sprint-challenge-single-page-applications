ac0aca252d7998a689dc266b71a9f01e351e9a0e
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-06-19 08:58:32 -0700

    Add home page with home and pizza routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Switch, Route, Link } from "react-router-dom";
+import './App.css';
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <nav>
+        <h1 className='store-header'>Lambda Eats</h1>
+        <div className='nav-links'>
+          <Link to='/'>Home</Link>
+        </div>
+      </nav>
+      <section className="pizza-section">
+        <h2>Your favorite food, delivered while coding</h2>
+        <Link to="/pizza">Pizza?</Link>
+      </section>
+
+      <Switch>
+        <Route></Route>
+        <Route path="/"></Route>
+      </Switch>
     </>
   );
 };
